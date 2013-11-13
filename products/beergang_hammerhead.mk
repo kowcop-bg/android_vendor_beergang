a37e66e9b9f53d097fef831d9ad278b7c9b43789
benjamingoodwin <benjamin.j.goodwin@gmail.com>
2013-11-13 16:46:40 +0000

    Build changes for BeerGang

--- products/beergang_hammerhead.mk
+++ products/beergang_hammerhead.mk
@@ -13,6 +13,9 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="fingerprint: google/hammerhead/hammerhead:4.4/KRT16M/893803:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4 KRT16M 893803 release-keys"
 
+PRODUCT_COPY_FILES += \
+    vendor/beergang/prebuilt/common/bootanimation/1080.zip:system/media/bootanimation.zip
+
 # Build SimToolKit
 PRODUCT_PACKAGES += \
     Stk
