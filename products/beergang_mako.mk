aac8182c39dbcded055b4a219cffb4bc1c8b7f02
benjamingoodwin <benjamin.j.goodwin@gmail.com>
2013-11-20 19:02:12 +0000

    Push alpha 0.4

--- products/beergang_mako.mk
+++ products/beergang_mako.mk
@@ -11,7 +11,7 @@ PRODUCT_DEVICE := mako
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/mako:4.4/KRT16O/893803:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.4 KRT16O 893803 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/mako:4.4/KRT16S/920375:user/release-keys" PRIVATE_BUILD_DESC="razor-user 4.4 KRT16S 920375 release-keys"
 
 PRODUCT_COPY_FILES += \
     vendor/beergang/prebuilt/common/bootanimation/1080.zip:system/media/bootanimation.zip
